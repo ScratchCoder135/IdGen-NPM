e74d427e13d3c03000da1c847b8e14759b0cda86
S Studio <lwb141424@gmail.com>
2022-09-09 20:24:03 +0800

    a

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var IdGenerator=function(){throw new Error("This package is under maintainence,please try again later.")}
+var IdGenerator=function(){throw new Error("This package is under maintainence,please try again later.You can find the details via npmjs.com/package/sec-id")}
 /*
 IdGenerator.prototype.GenerateNumberId=function(data){
     if(data.length!==null&&data.length!==undefined&&data.length!=="null"){
